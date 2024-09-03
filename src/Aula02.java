c74da919dd8f895d61e4a7144c598f100c20109c
thalisondev <contato@thalisonsilva.com.br>
2024-09-03 16:12:55 -0300

    Add: Aul02

---
+++ src/Aula02.java
@@ -0,0 +1,18 @@
+public class Aula02 {
+    public static void main(String[] args) {
+            // Declaração de variáveis
+            int idade = 25; // Tipo inteiro
+            double altura = 1.75; // Tipo ponto flutuante
+            char inicial = 'J'; // Tipo caractere
+            boolean estudante = true; // Tipo booleano
+            String nome = "João"; // Tipo String (sequência de caracteres)
+
+            // Uso das variáveis
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Altura: " + altura + " metros");
+            System.out.println("Inicial do nome: " + inicial);
+            System.out.println("É estudante? " + estudante);
+
+    }
+}
